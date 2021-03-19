fe71e7e198132f1d9d43677f559749b0385f234d
xiaojun <xiaojun@fulan.com.cn>
2021-03-19 18:14:39 +0800

    add commit

--- week001/week001/src/edu/geekbang/xmm/Hello.java
+++ week001/week001/src/edu/geekbang/xmm/Hello.java
@@ -6,20 +6,29 @@ package edu.geekbang.xmm;
 public class Hello {
 
     public static void main(String[] args) {
-        int a = 1;
+//        int a = 1;
+//
+//        int b = 2;
+//
+//        int c = a + b;
+//
+//        boolean d = a > b;
 
-        int b = 2;
+        test(1,1,1,1,1,1,1,1,1,1);
 
-        boolean c = a > b;
+//
+//        if (b > a) {
+//            int d = b - a;
+//        }
+//
+//        String[] e = { "hello", "word" };
+//        for (String str : e) {
+//            System.out.println(str);
+//        }
+    }
 
-        if (b > a) {
-            int d = b - a;
-        }
-
-        String[] e = { "hello", "word" };
-        for (String str : e) {
-            System.out.println(str);
-        }
+    private static void test(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j) {
+        System.out.println(a + b + c + d + e + f + g + h + i + j);
     }
 
 }
